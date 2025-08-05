6e2cff075cc7e7685e33c9d303d48e64faaabda6
Puhkusarvikuono <jyrki.vassinen@proton.me>
2025-08-05 11:39:44 +0300

    pygame.clock and tick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.time import Clock
 from constants import *
 
 def main():
@@ -7,13 +8,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock() 
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
-
+        clock.tick(60)
+        dt = clock.get_time() / 1000
 
 if __name__ == "__main__":
     main()
